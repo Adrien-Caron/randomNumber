48f65b444641fc2e82c368d03d247918ccef40ab
Adrien Caron <adrien62360@outlook.fr>
2024-04-29 13:05:44 +0200

    update app.js

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
-console.log('je suis trop fort');
-
 const config = {
-    mysteryNumber : [1, 20] ,
-    maxAttempts : 10,
-    attempt:[] 
+    mysteryNumber: [1,100],
+    maxAttempts: 10,
+    attempt: []
 }
 
-//Generer le numero mystere grace au mysyteryNumber qui se trouve dans config 
+// Générer un nombre mystère grace au mystereNumber
 const mysteryNumber = randomNumber()
-console.log('le nombre mystere est : ' ,mysteryNumber)
+console.log("Random number : ", mysteryNumber)
 
-// selon le nombre d'essais maxAttemps, on va incrementer dans attempt
-for (let i = 0; i < config.maxAttempts; i++) {
-    const numberRandom = randomNumber()
-    config.attempt.push(numberRandom)
+let found = false; // Variable pour indiquer si le nombre mystère a été trouvé
+
+// Selon le nombre d'essais maxAttempts, on va incrémenter dans attempt
+for (let i = 0; i < config.maxAttempts && !found; i++) { // Ajout de !found pour arrêter si le nombre mystère est trouvé
+    const numberRandom = randomNumber();
+    config.attempt.push(numberRandom);
+    if (numberRandom === mysteryNumber) {
+        found = true; // Mettre found à true si le nombre mystère est trouvé
+    }
 }
-console.log( config.attempt);
+console.log(config.attempt);
 
-// on vas comparer pour chaque attempt avec le numero mystere (si c'est gagnant) et indiquer lorsqu'il n'est pas gagnant 
-config.attempt.forEach((number, i)=>{
-    if (number < mysteryNumber){
-        console.log('et nan cest plus')
+
+// Comparer pour chaque attemp avec le numéro mystère si égalité = gagnant sinon perdu et indiquer si le numéro mystère est plus grand ou plus petit
+
+config.attempt.forEach((number, i)=> {
+    if(number < mysteryNumber){
+        console.log("Number +")
     }
-    if (number > mysteryNumber){
-        console.log('et nan cest moin')
+    if(number > mysteryNumber){
+        console.log("Number -")
     }
-    if (number === mysteryNumber){
-        console.log(`bravo, le numero mystere etait ${mysteryNumber},vous avez réussi en ${i+1} coups`)
+    if(number === mysteryNumber){
+        console.log(`Nice, the mystery number is ${mysteryNumber}, you have win in ${i + 1} attempts`);
+        // Si le nombre mystère est trouvé, sortir de la boucle forEach
+        return;
     }
-})
+});
 
-// si on gagne on affiche le numero mystere, avec le nombre d'essais
-
-
-
-function randomNumber(){
+function randomNumber() {
     const mysteryNumber = Math.floor(Math.random() * (config.mysteryNumber[1] - config.mysteryNumber[0] + 1)) + config.mysteryNumber[0];
-    return mysteryNumber
+    //console.log("Random number : ", mysteryNumber)
+    return mysteryNumber;
 }
